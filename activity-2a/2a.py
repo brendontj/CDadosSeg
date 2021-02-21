4deada48089083e5d35887f6b73ec8f354989ee9
Brendon Henrique <brendonhps@gmail.com>
2021-02-21 03:53:36 -0300

    part 1 done

--- activity-2a/2a.py
+++ activity-2a/2a.py
@@ -1,28 +1,44 @@
 import os
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as Et
 
 
 def print_response(apk_permission):
-    print("==============================================\n\nPermissões por APK\n\n==============================================\n\n")
+    print("=======\n\nPermissões por APK\n\n=======\n\n")
     for k, v in apk_permission.items():
-        print(k[16:-4] + str(':') + list(v))
+        print(k[16:-4] + str(':  ') + str(list(v)) + '\n\n')
+
+
+def print_1b(apk_permission):
+    print("=======\n\nPermissões únicas por APK\n\n=======\n\n")
+    repeated_permissions = set()
+    for k, v in apk_permission.items():
+        unique_permission = v.copy()
+        for ki, vi in apk_permission.items():
+            if k == ki:
+                continue
+            unique_permission -= v & vi
+        repeated_permissions.update(v - unique_permission)
+        print(k[16:-4] + str(':  ') + str(list(unique_permission)) + '\n\n')
+    print("=======\n\nPermissões comuns das APKs\n\n=======\n\n")
+    print(str(list(repeated_permissions)))
 
 
 def main():
     apk_permission = {}
     with os.scandir('manifests/') as entries:
         for entry in entries:
-            apk_permission[entry] = set()
-            root = ET.parse(entry).getroot()
+            apk_permission[entry.name] = set()
+            root = Et.parse(entry).getroot()
             permissions = root.findall("uses-permission")
             for perm in permissions:
                 for att in perm.attrib:
                     try:
-                        apk_permission[entry].add(perm.attrib[att].split('permission.')[1])
+                        apk_permission[entry.name].add(perm.attrib[att].split('permission.')[1])
                     except IndexError:
                         continue
 
-    print_response(apk_permission)
+    # print_response(apk_permission)
+    print_1b(apk_permission)
 
 
 if __name__ == '__main__':
